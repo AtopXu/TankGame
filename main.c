615fd45613d917f7f09df54cc9fbb4e3f117ba0e
AtopXu <m18251817876@163.com>
2017-05-31 23:28:40 +0800

    2

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include "acllib.h"
 
+int Setup()
+{
+	initWindow("Test",100,100,800,600);
+	
+	initConsole();
+	printf("hello\n");
+	
+	beginPaint();
+	line(10,10,100,100);
+	
+	endPaint();
+	
+	return 0;
+	
+ } 
